ade474399e81c29c761f4d752858b1cf84959a1f
ElisabethBrocca <elisabeth.brocca.07@gmail.com>
2025-10-08 16:11:37 +0200

    [UPD] EX-1-4

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -23,9 +23,10 @@ const lastTeacher = teachers[teachers.length];
 teachers.pop();
 console.log(teachers);
 
-
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers[0];
+teachers.shift()
+console.log(teachers);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 
